719679c7b3c0bb396f23dbf20c0617fc5c92ed11
salaamii <salamiolamide10@gmail.com>
2026-02-07 12:14:46 +0100

    nav-bar functionality

--- script.js
+++ script.js
@@ -2,12 +2,29 @@ const cartIndicator = document.getElementById("cart-logo");
 const cartHouse = document.getElementById("cart-house");
 const removeCart = document.getElementById("close-cart");
 
+
 cartIndicator.addEventListener("click" , () => {
     cartHouse.classList.add ("cart-display");
 });
 
 removeCart.addEventListener("click", () => {
     cartHouse.classList.remove ("cart-display");
+});
+
+
+const navbar = document.getElementById("close-nav");
+const navList = document.getElementById("nav-list");
+
+navbar.addEventListener("click" , () => {
+    navList.classList.toggle ("nav-display");
+
+    if (navList.classList.contains("nav-display")) {
+         navbar.innerHTML = `<i class="fa-solid fa-xmark fa-2xl" style="color: #ffffff;">`;
+
+    } else {
+        navbar.innerHTML = `<i class="fa-solid fa-bars fa-2xl" style="color: #ffffff;"></i>`;
+    }
+   
 });
 
 
